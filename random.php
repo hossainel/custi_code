712ad50ea467f82648566e14a4a3f590f31c8397
Md El Hossain <31356820+hossainel@users.noreply.github.com>
2022-09-02 06:20:09 +0600

    Add files via upload

---
+++ random.php
@@ -0,0 +1,25 @@
+<?php
+	//start a session
+	session_start();
+	//connect to the database
+	$conn = mysqli_connect("localhost","user","password","database");
+	//GCLID
+	$gclid = $_GET['gclid'];
+	//Making Random string
+	$r_str = '';
+	//letters
+	foreach (range('a', 'z') as $char) {
+		$r_str .= $char;
+	}
+	//numbers
+	for($i = 0; $i < 10; $i++){
+		$r_str .= $i;
+	}
+	//Generating random 8 char long string
+	$random = substr(str_shuffle($r_str), 0, 8);
+	//update the information into a database
+	$sql = "INSERT INTO `the_table_name`(`gclid`, `random`) VALUES ('$gclid', '$random');";
+	$conn->query($sql);
+	//insearting into a session
+	$_SESSION[$gclid] = $random;
+?>
